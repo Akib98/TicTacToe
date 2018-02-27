31c8005675a0abbf937407265e4586ab1c2562fb
Akib Ibn Zahid <akibibnzahid@gmail.com>
2018-02-27 11:33:24 +0600

    Setting up the grids

---
+++ TicTacToeUnity/Assets/Scripts/GridSpace.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridSpace : MonoBehaviour
+{
+
+    public Button button;
+    public Text buttonText;
+    public string playerSide;
+
+    public void SetSpace()
+    {
+        buttonText.text = playerSide;
+        button.interactable = false;
+    }
+}
